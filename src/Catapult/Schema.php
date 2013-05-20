119e3db2e37338873f5288e074b1159e0441cfab
pincombe <pincombe@gmail.com>
2013-05-21 00:18:28 +0200

    Update Schema.php

--- src/Catapult/Schema.php
+++ src/Catapult/Schema.php
@@ -9,7 +9,7 @@ class Schema
     public static function __callStatic($name, $arguments)
     {
         $db = new DB;
-        $schema->connection()->schema();
+        $schema = $db->connection()->schema();
         return call_user_func_array(array($schema, $name), array_values($arguments));
     }
 
