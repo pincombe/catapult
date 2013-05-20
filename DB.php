fd05655ec4f81bb24120e213bac95bb3b4576dbe
pincombe <pincombe@gmail.com>
2013-05-20 23:35:28 +0200

    Create DB.php

---
+++ DB.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace catapult;
+
+class DB
+{
+
+    static $conn;
+
+    public static function make($config)
+    {
+        self::$conn = new Illuminate\Database\Capsule($config);
+        self::$conn->bootEloquent();
+    }
+
+
+    public function __call($name, $args)
+    {
+        return call_user_func_array(array(self::$conn, $name), array_values($args));
+    }
+
+}
